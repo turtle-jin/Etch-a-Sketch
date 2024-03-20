d3f22f8fb9651a7674e66e19e437837243bdcadb
turtle-jin <codingjinjin@gmail.com>
2024-03-19 21:54:48 -0500

    update button evenlisteners

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const value = document.querySelector("#value");
 const userInput = document.querySelector("#user_input");
 const gridContainer = document.querySelector(".gridContainer");
+const colorSelector = document.querySelector("#colorSelector");
+const rainbowButton = document.querySelector("#rainbowButton");
+const resetButton = document.querySelector("#resetButton");
+const eraserButton = document.querySelector("#eraserButton");
 
-
+let isRainbowMode = false;
 let gridSize = userInput.value;
 value.textContent = `${gridSize} x ${gridSize}`;
 
@@ -16,6 +20,13 @@ function drawGrid(size) {
             cell.style.width = `${cellWidth}px`;
             cell.style.height = `${cellWidth}px`;
             cell.classList.add("cell");
+            cell.addEventListener("click", () => {
+                if (!eraserButton.classList.contains("active")) {
+                    cell.style.backgroundColor = colorSelector.value;
+                } else {
+                    cell.style.backgroundColor = 'antiquewhite';
+                }
+            });
             gridContainer.appendChild(cell);
         }
     }
@@ -26,6 +37,49 @@ userInput.addEventListener("input", (event) => {
     value.textContent = `${gridSize} x ${gridSize}`;
     drawGrid(gridSize);
 });
+
+rainbowButton.addEventListener("click", () => {
+    isRainbowMode = !isRainbowMode;
+    rainbowButton.textContent = isRainbowMode ? "Stop Rainbow" : "Rainbow Color";
+    if (isRainbowMode) {
+        console.log("rainbow mode on");
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.addEventListener("mouseover", rainbowMouseOver);
+        });
+    } else {
+        console.log("rainbow mode off");
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.removeEventListener("mouseover", rainbowMouseOver);
+        });
+    }
+});
+
+function rainbowMouseOver() {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    this.style.backgroundColor = randomColor;
+}
+
+resetButton.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "antiquewhite";
+    });
+});
+
+eraserButton.addEventListener("click", () => {
+    eraserButton.classList.toggle("active");
+    if (eraserButton.classList.contains("active")) {
+        eraserButton.textContent = "Draw"
+    } else {
+        eraserButton.textContent = "Eraser";
+    }
+    
+});
+
+
+
 
 
 drawGrid(gridSize); 
